b077eb4c4a48133778c163ab3e820149d0a6ee15
juji <jujiyangasli@gmail.com>
2025-12-20 09:09:48 +0700

    refactor: simplify vista-image.ts and reduce file size
    - Extract createImgElement and createPictureElement helpers
    - Consolidate getStyles logic using recursive approach
    - Extract getPictureSources helper to avoid duplication
    - Simplify getHiResElm and getThumbElm with early returns
    - Reduce constructImageElm by handling picture element first
    - Reduce file size by ~64 lines (~20% smaller)

--- src/lib/vista-image.ts
+++ src/lib/vista-image.ts
@@ -57,48 +57,52 @@ export class VistaImage {
     this.imageElm = null!;
   }
 
+  private createImgElement(attr: VistaImgAttr): HTMLImageElement {
+    const img = document.createElement('img');
+    img.src = attr.src;
+    if (attr.alt) img.alt = attr.alt;
+    if (attr.srcSet) img.srcset = attr.srcSet;
+    if (attr.sizes) img.sizes = attr.sizes;
+    return img;
+  }
+
+  private createPictureElement(sources: VistaPicAttr['sources'], alt?: string): HTMLPictureElement {
+    const picture = document.createElement('picture');
+
+    sources.forEach((sourceData) => {
+      const source = document.createElement('source');
+      source.srcset = sourceData.srcSet;
+      if (sourceData.media) source.media = sourceData.media;
+      if (sourceData.type) source.type = sourceData.type;
+      picture.appendChild(source);
+    });
+
+    const img = document.createElement('img');
+    img.src = sources.length > 0 ? sources[sources.length - 1].srcSet : '';
+    if (alt) img.alt = alt;
+    picture.appendChild(img);
+
+    return picture;
+  }
+
   getHiResElm(): HTMLImageElement | HTMLPictureElement {
     const elm = this.imageElm;
     const img = elm.img!;
     const pict = elm.picture!;
 
     // return existing hi-res element if already constructed
-    if (elm.type === 'img' && img.elm) {
-      return img.elm;
-    } else if (elm.type === 'picture' && pict.elm) {
-      return pict.elm;
-    }
+    if (elm.type === 'img' && img.elm) return img.elm;
+    if (elm.type === 'picture' && pict.elm) return pict.elm;
 
     // construct hi-res element
     if (elm.type === 'img') {
-      const im = document.createElement('img');
-      im.src = img.src;
+      img.elm = this.createImgElement(img);
+      return img.elm;
+    }
 
-      if (img.alt) im.alt = img.alt!;
-      if (img.srcSet) im.srcset = img.srcSet!;
-      if (img.sizes) im.sizes = img.sizes!;
-
-      img.elm = im;
-      return im;
-    } else if (elm.type === 'picture') {
-      const picture = document.createElement('picture');
-      const sources = elm.picture!.sources;
-
-      sources.forEach((sourceData) => {
-        const source = document.createElement('source');
-        source.srcset = sourceData.srcSet;
-        if (sourceData.media) source.media = sourceData.media;
-        if (sourceData.type) source.type = sourceData.type;
-        picture.appendChild(source);
-      });
-
-      const im = document.createElement('img');
-      im.src = sources.length > 0 ? sources[sources.length - 1].srcSet : '';
-      if (pict.img?.alt) im.alt = pict.img.alt;
-      picture.appendChild(im);
-
-      pict.elm = picture;
-      return picture;
+    if (elm.type === 'picture') {
+      pict.elm = this.createPictureElement(pict.sources, pict.img?.alt);
+      return pict.elm;
     }
 
     throw new Error('Unsupported hi-res element type for VistaImage.');
@@ -108,45 +112,19 @@ export class VistaImage {
     const elm = this.imageElm;
 
     // return existing thumb element if already constructed
-    if (elm.thumb!.elm) {
+    if (elm.thumb!.elm) return elm.thumb!.elm;
+
+    // construct thumb element
+    if (elm.type === 'img') {
+      const thumbAttr = elm.thumb!.attributes as VistaImgAttr;
+      elm.thumb!.elm = this.createImgElement(thumbAttr);
       return elm.thumb!.elm;
     }
 
-    // construct thumb element
-
-    if (elm.type === 'img') {
-      const thumbAttr = elm.thumb!.attributes as VistaImgAttr;
-      const img = document.createElement('img');
-      img.src = thumbAttr.src;
-
-      if (thumbAttr.alt) img.alt = thumbAttr.alt!;
-      if (thumbAttr.srcSet) img.srcset = thumbAttr.srcSet!;
-      if (thumbAttr.sizes) img.sizes = thumbAttr.sizes!;
-
-      elm.thumb!.elm = img;
-      return img;
-    } else if (elm.type === 'picture') {
+    if (elm.type === 'picture') {
       const thumbAttr = elm.thumb!.attributes as VistaPicAttr;
-      const picture = document.createElement('picture');
-      const sources = thumbAttr.sources;
-      const imageAlt = thumbAttr.img.alt || '';
-
-      sources.forEach((sourceData) => {
-        const source = document.createElement('source');
-        source.srcset = sourceData.srcSet;
-        if (sourceData.media) source.media = sourceData.media;
-        if (sourceData.type) source.type = sourceData.type;
-        picture.appendChild(source);
-      });
-
-      const img = document.createElement('img');
-      img.src = sources.length > 0 ? sources[sources.length - 1].srcSet : '';
-      img.alt = imageAlt;
-
-      picture.appendChild(img);
-
-      elm.thumb!.elm = picture;
-      return picture;
+      elm.thumb!.elm = this.createPictureElement(thumbAttr.sources, thumbAttr.img.alt);
+      return elm.thumb!.elm;
     }
 
     throw new Error('Unsupported thumb element type for VistaImage.');
@@ -156,61 +134,28 @@ export class VistaImage {
     borderRadius?: string;
     objectFit?: string;
   } {
-    if (elm instanceof HTMLImageElement) {
-      const borderRadius = getComputedStyle(elm).borderRadius;
+    const borderRadius = getComputedStyle(elm).borderRadius;
 
-      if (isNotZeroCssValue(borderRadius)) {
-        return {
-          borderRadius: borderRadius,
-          objectFit: getComputedStyle(elm).objectFit,
-        };
-      }
-
-      return {};
-    } else if (elm instanceof HTMLPictureElement) {
-      const elmBorderRadius = getComputedStyle(elm).borderRadius;
-
-      if (isNotZeroCssValue(elmBorderRadius)) {
-        return {
-          borderRadius: elmBorderRadius,
-          objectFit: getComputedStyle(elm).objectFit,
-        };
-      }
-
-      const image = elm.querySelector('img') as HTMLImageElement | null;
-      const styles = image ? this.getStyles(image) : { borderRadius: '', objectFit: '' };
-
-      if (styles.borderRadius && isNotZeroCssValue(styles.borderRadius)) {
-        return styles;
-      }
-
-      return {};
-    } else if (elm instanceof HTMLAnchorElement) {
-      const elmBorderRadius = getComputedStyle(elm).borderRadius;
-
-      if (isNotZeroCssValue(elmBorderRadius)) {
-        return {
-          borderRadius: elmBorderRadius,
-          objectFit: getComputedStyle(elm).objectFit,
-        };
-      }
-
-      const imgChild = elm.querySelector('img') as HTMLImageElement | null;
-      let styles = imgChild ? this.getStyles(imgChild) : { borderRadius: '', objectFit: '' };
-      if (styles.borderRadius && isNotZeroCssValue(styles.borderRadius)) {
-        return styles;
-      }
-
-      const pictureChild = elm.querySelector('picture') as HTMLPictureElement | null;
-      styles = pictureChild ? this.getStyles(pictureChild) : { borderRadius: '', objectFit: '' };
-      if (styles.borderRadius && isNotZeroCssValue(styles.borderRadius)) {
-        return styles;
-      }
-
-      return {};
+    if (isNotZeroCssValue(borderRadius)) {
+      return {
+        borderRadius,
+        objectFit: getComputedStyle(elm).objectFit,
+      };
     }
 
+    // Check child elements
+    const child = elm.querySelector('img, picture') as HTMLImageElement | HTMLPictureElement | null;
+    if (child) return this.getStyles(child);
+
     return {};
+  }
+
+  private getPictureSources(pictureElm: HTMLPictureElement) {
+    return Array.from(pictureElm.querySelectorAll('source')).map((source) => ({
+      srcSet: source.srcset,
+      media: source.media,
+      type: source.type,
+    }));
   }
 
   private constructImageElm(
@@ -241,94 +186,22 @@ export class VistaImage {
           sizes: elm.dataset.vistaviewSizes || elm.sizes,
         },
       };
-    } else if (elm instanceof HTMLAnchorElement) {
-      const imgChild = elm.querySelector('img') as HTMLImageElement | null;
-      const pictureChild = elm.querySelector('picture') as HTMLPictureElement | null;
+    }
 
-      if (imgChild) {
-        return {
-          type: 'a',
-          childElm: 'img',
-          anchor: {
-            elm: elm,
-          },
-          thumb: {
-            origin: imgChild,
-            attributes: {
-              src: imgChild.src,
-              alt: imgChild.alt,
-              srcSet: imgChild.srcset,
-              sizes: imgChild.sizes,
-            },
-            width: imgChild.width,
-            height: imgChild.height,
-            borderRadius: styles.borderRadius || '',
-            objectFit: styles.objectFit || '',
-          },
-          img: {
-            src: elm.dataset.vistaviewSrc || elm.getAttribute('href') || imgChild.src,
-            alt: elm.dataset.vistaviewAlt || imgChild.alt,
-            srcSet: elm.dataset.vistaviewSrcSet || imgChild.srcset,
-            sizes: elm.dataset.vistaviewSizes || imgChild.sizes,
-          },
-        };
-      } else if (pictureChild) {
-        const image = pictureChild.querySelector('img') as HTMLImageElement | null;
-        const sources = elm.dataset.vistaviewSources
-          ? JSON.parse(elm.dataset.vistaviewSources)
-          : null;
-        const pictSources = Array.from(pictureChild.querySelectorAll('source')).map((source) => ({
-          srcSet: source.srcset,
-          media: source.media,
-          type: source.type,
-        }));
-
-        return {
-          type: 'a',
-          childElm: 'picture',
-          anchor: {
-            elm: elm,
-          },
-          thumb: {
-            origin: pictureChild,
-            attributes: {
-              sources: pictSources,
-              img: {
-                alt: (image ? image.alt : '') || elm.dataset.vistaviewAlt,
-              },
-            },
-            width: image?.width || 0,
-            height: image?.height || 0,
-            borderRadius: styles.borderRadius || '',
-            objectFit: styles.objectFit || '',
-          },
-          picture: {
-            sources: sources || pictSources,
-            img: {
-              alt: elm.dataset.vistaviewAlt || (image ? image.alt : ''),
-            },
-          },
-        };
-      }
-    } else if (elm instanceof HTMLPictureElement) {
+    if (elm instanceof HTMLPictureElement) {
       const image = elm.querySelector('img') as HTMLImageElement | null;
       const sources = elm.dataset.vistaviewSources
         ? JSON.parse(elm.dataset.vistaviewSources)
-        : null;
-      const pictSources = Array.from(elm.querySelectorAll('source')).map((source) => ({
-        srcSet: source.srcset,
-        media: source.media,
-        type: source.type,
-      }));
+        : this.getPictureSources(elm);
 
       return {
         type: 'picture',
         thumb: {
           origin: elm,
           attributes: {
-            sources: pictSources,
+            sources: this.getPictureSources(elm),
             img: {
-              alt: (image ? image.alt : '') || elm.dataset.vistaviewAlt,
+              alt: image?.alt || '' || elm.dataset.vistaviewAlt,
             },
           },
           width: image?.width || 0,
@@ -337,9 +210,72 @@ export class VistaImage {
           objectFit: styles.objectFit || '',
         },
         picture: {
-          sources: sources || pictSources,
+          sources,
           img: {
-            alt: elm.dataset.vistaviewAlt || (image ? image.alt : ''),
+            alt: elm.dataset.vistaviewAlt || image?.alt || '',
+          },
+        },
+      };
+    }
+
+    // Handle anchor element
+    const imgChild = elm.querySelector('img') as HTMLImageElement | null;
+    const pictureChild = elm.querySelector('picture') as HTMLPictureElement | null;
+
+    if (imgChild) {
+      return {
+        type: 'a',
+        childElm: 'img',
+        anchor: { elm },
+        thumb: {
+          origin: imgChild,
+          attributes: {
+            src: imgChild.src,
+            alt: imgChild.alt,
+            srcSet: imgChild.srcset,
+            sizes: imgChild.sizes,
+          },
+          width: imgChild.width,
+          height: imgChild.height,
+          borderRadius: styles.borderRadius || '',
+          objectFit: styles.objectFit || '',
+        },
+        img: {
+          src: elm.dataset.vistaviewSrc || elm.getAttribute('href') || imgChild.src,
+          alt: elm.dataset.vistaviewAlt || imgChild.alt,
+          srcSet: elm.dataset.vistaviewSrcSet || imgChild.srcset,
+          sizes: elm.dataset.vistaviewSizes || imgChild.sizes,
+        },
+      };
+    }
+
+    if (pictureChild) {
+      const image = pictureChild.querySelector('img') as HTMLImageElement | null;
+      const sources = elm.dataset.vistaviewSources
+        ? JSON.parse(elm.dataset.vistaviewSources)
+        : this.getPictureSources(pictureChild);
+
+      return {
+        type: 'a',
+        childElm: 'picture',
+        anchor: { elm },
+        thumb: {
+          origin: pictureChild,
+          attributes: {
+            sources: this.getPictureSources(pictureChild),
+            img: {
+              alt: image?.alt || '' || elm.dataset.vistaviewAlt,
+            },
+          },
+          width: image?.width || 0,
+          height: image?.height || 0,
+          borderRadius: styles.borderRadius || '',
+          objectFit: styles.objectFit || '',
+        },
+        picture: {
+          sources,
+          img: {
+            alt: elm.dataset.vistaviewAlt || image?.alt || '',
           },
         },
       };
